751f824ab17901d7aa068796ad8606bc5a649c2d
vruban <vova.ruban9@gmail.com>
2020-11-15 13:50:38 +0300

    created component BoxPartnership

--- src/components/content/BoxPartnership.tsx
+++ src/components/content/BoxPartnership.tsx
@@ -2,20 +2,38 @@ import React from 'react';
 
 import Text from '../Text';
 import images from '../../images';
+import BacrgroundLeaf from './BacrgroundLeaf';
 
 import styles from './BoxPartnership.module.css';
 
-const Box = () => {
+const data = [
+  {
+    img: images.photo1,
+    title: 'Коворкинг и акселераторы',
+    text:
+      'Узнайте больше о том, как наши партнеры по совместной работе и ускорению в Великобритании помогают революционным предпринимателям привлекать финансовые средства посредством краудфандинга.',
+  },
+  {
+    img: images.photo2,
+    title: 'Корпоративные финансы',
+    text:
+      'Узнайте больше о том, как наши партнеры по совместной работе и ускорению в Великобритании помогают революционным предпринимателям привлекать финансовые средства посредством краудфандинга.',
+  },
+];
+
+type boxProps = {
+  img: string;
+  text: string;
+  title: string;
+};
+
+const Box: React.FC<boxProps> = ({ img, text, title }) => {
   return (
     <div className={styles.box}>
-      <img src={images.photo1} alt="" />
+      <img src={img} alt="" />
       <div className={styles.body}>
-        <Text bold> Коворкинг и акселераторы</Text>
-        <Text classNames={styles.text}>
-          Узнайте больше о том, как наши партнеры по совместной работе и
-          ускорению в Великобритании помогают революционным предпринимателям
-          привлекать финансовые средства посредством краудфандинга.
-        </Text>
+        <Text bold>{title}</Text>
+        <Text classNames={styles.text}>{text}</Text>
       </div>
     </div>
   );
@@ -28,8 +46,12 @@ const BoxPartnership = () => {
         Какое это партнёрство
       </Text>
       <div className={styles.boxs}>
-        <Box />
-        <Box />
+        {data.map((i, index) => (
+          <Box key={index} img={i.img} text={i.text} title={i.title} />
+        ))}
+      </div>
+      <div className={styles.leaf}>
+        <BacrgroundLeaf />
       </div>
     </div>
   );
