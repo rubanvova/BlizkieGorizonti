277813cc50e371532801d62311cd711da5d42ad3
vruban <vova.ruban9@gmail.com>
2020-11-18 11:05:36 +0300

    created component BoxLogIn

--- src/components/header/BoxLogIn.tsx
+++ src/components/header/BoxLogIn.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
+import images from '../../images';
+
 import styles from './BoxLogIn.module.css';
 
 const BoxLogIn: React.FC = () => {
-  return <div className={styles.wrapper}>log in</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.img}>
+          <img src={images.logo} alt="" />
+        </div>
+        <div className={styles.users}>
+          <div className={styles.user}>
+            <div className={styles.userText}> Инвестору</div>
+          </div>
+          <div className={styles.user}>
+            <div className={styles.userText}> Гос услуги</div>
+          </div>
+        </div>
+        <div className={styles.inputs}>
+          <input className={styles.input} placeholder="EMAIL" />
+          <input className={styles.input} placeholder="PASSWORD" />
+        </div>
+        <div className={styles.boxButton}>
+          <button className={styles.button}> Войти</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BoxLogIn;
