7df56de103a9ca02cd1dcfb8c16fb85ff72fb5ce
vruban <vova.ruban9@gmail.com>
2020-11-19 12:53:37 +0300

    add modal form in component BoxForm

--- src/components/content/BoxForm.tsx
+++ src/components/content/BoxForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Text from '../Text';
 
@@ -6,19 +6,54 @@ import styles from './BoxForm.module.css';
 
 type inputProps = {
   title: string;
+  name: string;
   place: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => any;
 };
 
-const Input: React.FC<inputProps> = ({ title, place }) => {
+const Input: React.FC<inputProps> = ({ title, name, place, onChange }) => {
   return (
     <div>
       <Text classNames={styles.title}>{title}</Text>
-      <input className={styles.input} placeholder={place} />
+      <input
+        name={name}
+        onChange={onChange}
+        className={styles.input}
+        placeholder={place}
+      />
     </div>
   );
 };
 
 const BoxForm: React.FC = () => {
+  type types = {
+    name: string;
+    tel: string;
+    email: string;
+    web: string;
+  };
+  const [showModal, setshowModal] = useState<boolean>(false);
+
+  const [dataInputs, setDataInputs] = useState<types>({
+    name: '',
+    tel: '',
+    email: '',
+    web: '',
+  });
+
+  const handlerChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (showModal) {
+      e.target.value = '';
+      console.log('123');
+    }
+    setDataInputs({ ...dataInputs, [e.target.name]: e.target.value });
+  };
+
+  const closeModal = () => {
+    setshowModal((modal) => !modal);
+    setDataInputs({ name: '', tel: '', email: '', web: '' });
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
@@ -34,17 +69,57 @@ const BoxForm: React.FC = () => {
         </div>
         <div className={styles.inputs}>
           <div className={styles.rowOne}>
-            <Input title="ФИО" place="Петров Иван Сергеевич" />
-            <Input title="Контактный телефон" place="+7 987 654 32 10" />
+            <Input
+              name="name"
+              onChange={handlerChange}
+              title="ФИО"
+              place="Петров Иван Сергеевич"
+            />
+            <Input
+              name="tel"
+              onChange={handlerChange}
+              title="Контактный телефон"
+              place="+7 987 654 32 10"
+            />
           </div>
           <div className={styles.rowTwo}>
-            <Input title="Электронная почта" place="example@mail.com" />
-            <Input title="Сайт компании" place="companyadress.com" />
+            <Input
+              name="email"
+              onChange={handlerChange}
+              title="Электронная почта"
+              place="example@mail.com"
+            />
+            <Input
+              name="web"
+              onChange={handlerChange}
+              title="Сайт компании"
+              place="companyadress.com"
+            />
           </div>
         </div>
         <div className={styles.boxButton}>
-          <button className={styles.button}>Стать партнёром</button>
+          <button
+            className={styles.button}
+            onClick={() => setshowModal(!showModal)}
+          >
+            Стать партнёром
+          </button>
         </div>
+        {showModal && (
+          <div className={styles.wrapperModal}>
+            <div className={styles.modal}>
+              <div className={styles.textModal}>
+                <Text>ФИО:{dataInputs.name} </Text>
+                <Text>Контактный телефон:{dataInputs.tel} </Text>
+                <Text>Электронная почта:{dataInputs.email} </Text>
+                <Text>Сайт компании:{dataInputs.web} </Text>
+                <button className={styles.buttonOk} onClick={closeModal}>
+                  OK
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
